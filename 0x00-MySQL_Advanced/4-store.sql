cbabba788a9083bee7157c9ef326a717acbf40c1
Grace_ngigi <gracengigi859@gmail.com>
2023-12-16 16:38:58 -0800

    try to correct trigger after inser

--- 0x00-MySQL_Advanced/4-store.sql
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -2,15 +2,11 @@
 -- item after adding a new order.
 -- Quantity in the table items can be negative.
 
-DELIMITER $$
+DELIMITER $$ ;
 
-USE `holberton`$$
-
-CREATE TRIGGER decr_items 
-AFTER INSERT ON `orders`
+CREATE TRIGGER decr_items AFTER INSERT ON orders
 FOR EACH ROW
 	BEGIN
 	UPDATE `items` SET quantity = quantity - NEW.number WHERE name=NEW.item_name;
-END$$
-
-DELIMITER ;
+END;$$
+delimiter ;
