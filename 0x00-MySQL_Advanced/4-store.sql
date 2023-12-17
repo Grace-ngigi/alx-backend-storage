25bf3322f5adcefbdbcc4acce5a14c4be0ec10e9
Grace_ngigi <gracengigi859@gmail.com>
2023-12-16 16:28:59 -0800

    try to correct trigger after inser

--- 0x00-MySQL_Advanced/4-store.sql
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -7,10 +7,10 @@ DELIMITER $$
 USE `holberton`$$
 
 CREATE TRIGGER decr_items 
-AFTER INSERT ON `holberton`.`orders`
+AFTER INSERT ON `orders`
 FOR EACH ROW
 	BEGIN
-	UPDATE `holberton`.`items` SET quantity = quantity - NEW.number WHERE name=NEW.item_name;
+	UPDATE `items` SET quantity = quantity - NEW.number WHERE name=NEW.item_name;
 END$$
 
 DELIMITER ;
