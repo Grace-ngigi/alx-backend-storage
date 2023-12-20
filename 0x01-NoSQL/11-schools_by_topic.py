9037e232c8388b1ae56e0763dd99cd60231a37cf
Grace_ngigi <gracengigi859@gmail.com>
2023-12-19 17:26:36 -0800

    find doc

---
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+''' find doc '''
+import pymongo
+
+
+def schools_by_topic(mongo_collection, topic):
+    return mongo_collection.find(
+            {"topics": topic}
+            )
